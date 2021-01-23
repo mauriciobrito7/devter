41f604f08b7685ec646d91786b6fe59559a77604
Mauricio <maubot01@gmail.com>
2021-01-23 12:57:54 -0400

    split styles

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,62 +1,15 @@
 import React from "react";
-import { breakpoints, fonts, colors } from "../../styles/theme";
-import { addOpacityToColor } from "../../styles/utils";
-
-const backgroundColor = addOpacityToColor(colors.primary, 0.5);
-
+import styles, { globalStyles } from "./AppLayout.styles";
 export const AppLayout = ({ children }) => {
-  console.log(backgroundColor);
   return (
     <>
       <div>
         <main>{children}</main>
       </div>
 
-      <style jsx>
-        {`
-          div {
-            display: grid;
-            height: 100vh;
-            place-items: center;
-          }
-
-          main {
-            background: #fff;
-            box-shadow: 0 10px 25px rgba(0, 0, 0, 0.1);
-            height: 90vh;
-            width: 450px;
-          }
-          @media (min-width: ${breakpoints.mobile}) {
-            main {
-              height: 90vh;
-              width: ${breakpoints.mobile};
-            }
-          }
-        `}
-      </style>
+      <style jsx>{styles}</style>
       <style jsx global>
-        {`
-          html,
-          body {
-            background-image: radial-gradient(
-                ${backgroundColor} 1px,
-                transparent 1px
-              ),
-              radial-gradient(${backgroundColor} 1px, transparent 1px);
-            background-position: 0 0, 25px 25px;
-            background-size: 50px 50px;
-            padding: 0;
-            margin: 0;
-            font-family: ${fonts.base};
-          }
-          * {
-            box-sizing: border-box;
-          }
-          a {
-            color: inherit;
-            text-decoration: none;
-          }
-        `}
+        {globalStyles}
       </style>
     </>
   );
---
+++ components/AppLayout/AppLayout.styles.js
@@ -0,0 +1,46 @@
+import css from "styled-jsx/css";
+import { breakpoints, fonts, colors } from "../../styles/theme";
+import { addOpacityToColor } from "../../styles/utils";
+
+const backgroundColor = addOpacityToColor(colors.primary, 0.5);
+
+export const globalStyles = css.global`
+  html,
+  body {
+    background-image: radial-gradient(${backgroundColor} 1px, transparent 1px),
+      radial-gradient(${backgroundColor} 1px, transparent 1px);
+    background-position: 0 0, 25px 25px;
+    background-size: 50px 50px;
+    padding: 0;
+    margin: 0;
+    font-family: ${fonts.base};
+  }
+  * {
+    box-sizing: border-box;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
+
+export default css`
+  div {
+    display: grid;
+    height: 100vh;
+    place-items: center;
+  }
+
+  main {
+    background: #fff;
+    box-shadow: 0 10px 25px rgba(0, 0, 0, 0.1);
+    height: 90vh;
+    width: 450px;
+  }
+  @media (min-width: ${breakpoints.mobile}) {
+    main {
+      height: 90vh;
+      width: ${breakpoints.mobile};
+    }
+  }
+`;
