16321273fcb1182014f2484efcd2701374d45738
Mauricio <maubot01@gmail.com>
2021-02-11 11:40:17 -0400

    support for mobile

--- hooks/useTimeAgo.js
+++ hooks/useTimeAgo.js
@@ -1,25 +1,49 @@
+import { useEffect, useState } from "react";
+import { formatDate } from "./useDateTimeFormat";
+
+const isRelativeTimeFormatSupported =
+  typeof Intl !== "undefined" && Intl.RelativeTimeFormat;
+
 const DATE_UNITS = [
   ["day", 86400],
-  ["hours", 3600],
+  ["hour", 3600],
   ["minute", 60],
   ["second", 1],
 ];
 
-const getDateDiff = (timestamp) => {
+const getDateDiffs = (timestamp) => {
   const now = Date.now();
   const elapsed = (timestamp - now) / 1000;
+
   for (const [unit, secondsInUnit] of DATE_UNITS) {
     if (Math.abs(elapsed) > secondsInUnit || unit === "second") {
-      const value = Math.floor(elapsed / secondsInUnit);
+      const value = Math.round(elapsed / secondsInUnit);
       return { value, unit };
     }
   }
 };
 
 export default function useTimeAgo(timestamp) {
-  const { value, unit } = getDateDiff(timestamp);
-  const rtf = new Intl.RelativeTimeFormat("es", {
-    style: "short",
-  });
+  const [timeago, setTimeago] = useState(() => getDateDiffs(timestamp));
+
+  useEffect(() => {
+    if (isRelativeTimeFormatSupported) {
+      const interval = setInterval(() => {
+        const newTimeAgo = getDateDiffs(timestamp);
+        setTimeago(newTimeAgo);
+      }, 5000);
+
+      return () => clearInterval(interval);
+    }
+  }, [timestamp]);
+
+  if (!isRelativeTimeFormatSupported) {
+    return formatDate(timestamp);
+  }
+
+  const rtf = new Intl.RelativeTimeFormat("es", { style: "short" });
+
+  const { value, unit } = timeago;
+
   return rtf.format(value, unit);
 }
