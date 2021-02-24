a10688bd3e17fa1167257098f8aa3d44f4b6d046
Mauricio <maubot01@gmail.com>
2021-02-24 13:33:51 -0400

    puting nav on every page

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,10 +1,14 @@
+import { Nav } from "components/Nav/Nav";
 import React from "react";
 import styles, { globalStyles } from "./AppLayout.styles";
 export const AppLayout = ({ children }) => {
   return (
     <>
       <div>
-        <main>{children}</main>
+        <main>
+          {children}
+          <Nav />
+        </main>
       </div>
 
       <style jsx>{styles}</style>
