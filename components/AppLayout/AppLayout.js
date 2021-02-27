45d7f514897ed6c9a7876551909ad1ca6f32b18b
Mauricio <maubot01@gmail.com>
2021-02-27 18:18:15 -0400

    Footer

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,12 +1,15 @@
 import { Nav } from "components/Nav/Nav";
 import React from "react";
 import styles, { globalStyles } from "./AppLayout.styles";
+import Footer from "components/Footer/Footer";
+
 export const AppLayout = ({ children }) => {
   return (
     <>
       <div>
         <main>
           {children}
+          <Footer />
           <Nav />
         </main>
       </div>
